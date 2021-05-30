3691f4febdd602722652a2923e1666ca0a1da7a0
Your Username <you@example.com>
2021-05-29 22:26:04 -0400

    commit tree 1586fcbaeeb836b477d8558011728721031b28bf

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+function findMatching(drivers, name) { 
+   return drivers.filter(object => { 
+    return object.toLowerCase() === name.toLowerCase()
+        // we are going to have to match our name and the object, by making them both lowercase
+        // make sure that the object name and the name are similar, basically comparing them
+         })
+}
+
+
+
+
+function fuzzyMatch(drivers, letters) { 
+    return drivers.filter(function(name) {
+       return name.substring(0,letters.length) === letters 
+    })
+} 
+
+function matchName(drivers, letters) {
+    return drivers.filter(function(driver) {
+    return driver.name === letters})
+    }
+
+
